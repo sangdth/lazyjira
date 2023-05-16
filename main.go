872cb1135f5a07f23d81533f28f40be5cdd347cc
Sang Dang <sangdth@gmail.com>
2023-05-16 11:50:38 +0300

    replace print with log panic

--- main.go
+++ main.go
@@ -3,38 +3,46 @@ package main
 import (
 	"fmt"
 	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func main() {
 	// Initialize the gocui library
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		fmt.Println("Error initializing gocui library: ", err)
-		return
+		log.Panicln(err)
 	}
+
 	defer g.Close()
 
 	// Set up the main screen and keybindings
 	g.SetManagerFunc(layout)
-	if err := keybindings(g); err != nil {
-		fmt.Println("Error setting up keybindings: ", err)
-		return
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
 	}
 
 	// Start the main event loop
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		fmt.Println("Error in main event loop: ", err)
+		log.Panicln(err)
 	}
 }
 
 // The layout function sets up the main screen layout
 func layout(g *gocui.Gui) error {
-	// TODO: Implement the layout function
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("colors", maxX/2-7, maxY/2-12, maxX/2+7, maxY/2+13); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		for i := 0; i <= 7; i++ {
+			for _, j := range []int{1, 4, 7} {
+				fmt.Fprintf(v, "lazy he he \033[3%d;%dmcolors!\033[0m\n", i, j)
+			}
+		}
+	}
 	return nil
 }
 
-// The keybindings function sets up the main keybindings
-func keybindings(g *gocui.Gui) error {
-	// TODO: Implement the keybindings function
-	return nil
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
